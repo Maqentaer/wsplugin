2690a743248a76c40d2978ae59773dc230306fa7
salahov <am.salahov@gmail.com>
2015-05-24 21:29:58 +0400

    наработки для действия добавления контрола

--- src/ws/actions/WSAddComponent.java
+++ src/ws/actions/WSAddComponent.java
@@ -3,14 +3,37 @@ package ws.actions;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class WSAddComponent extends com.intellij.openapi.actionSystem.AnAction {
+    public static String jsFileString = "" +
+            "define('js!%1$s', [\n" +
+            "    'js!SBIS3.CORE.CompoundControl'\n" +
+            "], function( CompoundControl ) {\n" +
+            "\n" +
+            "    'use strict';\n" +
+            "\n" +
+            "    /**\n" +
+            "    *\n" +
+            "    * @class %1$s\n" +
+            "    * @extends $ws.proto.CompoundControl\n" +
+            "    */\n" +
+            "    var %2$s = CompoundControl.extend(/** @lends %1$s.prototype */{\n" +
+            "        $protected : {\n" +
+            "            _options: {}\n" +
+            "        },\n" +
+            "        $constructor: function (){},\n" +
+            "        init: function (){\n" +
+            "            %2$s.superclass.init.call(this);\n" +
+            "        }\n" +
+            "    });\n" +
+            "    return %2$s;\n" +
+            "});";
+    public static String xhtmlFileString = "<div class=\"%s\"></div>";
+    public static String cssFileString = ".%s{}";
+
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent) {
+    public void actionPerformed(final AnActionEvent anActionEvent) {
         //anActionEvent.getData();
-        new WSAddComponentDialog(){
-            private void onOK() {
-                dispose();
-            }
-        };
+
+        new WSAddComponentDialog(anActionEvent.getProject());
 
     }
 }
--- src/ws/actions/WSAddComponentDialog.java
+++ src/ws/actions/WSAddComponentDialog.java
@@ -1,9 +1,20 @@
 package ws.actions;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WSAddComponentDialog extends JDialog {
+    private final Project project;
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
@@ -12,7 +23,8 @@ public class WSAddComponentDialog extends JDialog {
     private JCheckBox checkXhtml;
     private JCheckBox checkCss;
 
-    public WSAddComponentDialog() {
+    public WSAddComponentDialog(Project project) {
+        this.project = project;
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
@@ -49,10 +61,50 @@ public class WSAddComponentDialog extends JDialog {
     }
 
     private void onOK() {
+        PsiDirectory directory = PsiManager.getInstance(project).findDirectory(project.getBaseDir());
+        if (directory != null && !getControlName().isEmpty()) {
+            Pattern pattern = Pattern.compile("^(SBIS3)\\.(\\w+)\\.(\\w+)");
+            Matcher matcher = pattern.matcher(getControlName());
+
+            if (matcher.find()) {
+                String prName = matcher.group(2);
+                String controlName = matcher.group(3);
+                try {
+                    PsiDirectory controlDirectory = directory.createSubdirectory(controlName);
+                    if (checkJs.isSelected()) {
+                        PsiFile jsFile = controlDirectory.createFile(controlName + ".module.js");
+
+                        FileWriter fileWriter = new FileWriter(jsFile.getVirtualFile().getPath());
+                        fileWriter.write(String.format(WSAddComponent.jsFileString, getControlName(), controlName));
+                        fileWriter.close();
+
+                    }
+                    if (checkXhtml.isSelected()) {
+                        PsiFile xhtmlFile = controlDirectory.createFile(controlName + ".xhtml");
+
+                        FileWriter fileWriter = new FileWriter(xhtmlFile.getVirtualFile().getPath());
+                        fileWriter.write(String.format(WSAddComponent.xhtmlFileString, prName.toLowerCase() + "-" + controlName.toLowerCase()));
+                        fileWriter.close();
+                    }
+                    if (checkJs.isSelected()) {
+                        PsiFile checkCss = controlDirectory.createFile(controlName + ".css");
+                        FileWriter fileWriter = new FileWriter(checkCss.getVirtualFile().getPath());
+                        fileWriter.write(String.format(WSAddComponent.cssFileString, prName.toLowerCase() + "-" + controlName.toLowerCase()));
+                        fileWriter.close();
+                    }
+                } catch (IOException e) {
+                }
+            }
+        }
+
         dispose();
     }
 
     private void onCancel() {
         dispose();
+    }
+
+    public String getControlName() {
+        return componentName.getText();
     }
 }
